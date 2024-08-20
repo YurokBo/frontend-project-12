ec103c74c3ccd95e600a497f9b7508b83d6b8d59
Yuriy Bondaryuk <ybondaryuk@marfa-tech.com>
2024-08-20 23:01:11 +0300

    main review: fix 7 - token to secret

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,5 +1,5 @@
 const rollbarConfig = {
-  accessToken: '23bd92fae990454b8789ad3be0601895' /* process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN */,
+  accessToken: process.env.POST_CLIENT_ITEM /* '23bd92fae990454b8789ad3be0601895' */,
   environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
