8d5408653f5153328edec975da041a67ea9af528
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-08-20 22:28:50 +0300

    main review: fix 6 - add init app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,46 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as StoreProvider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import leoProfanity from 'leo-profanity';
-import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store';
-import initSocket from './store/socket/socketApi';
-import i18next from './i18next';
-
-initSocket(store);
-
-leoProfanity.add(leoProfanity.getDictionary('ru'));
-leoProfanity.add(leoProfanity.getDictionary('en'));
-
-const rollbarConfig = {
-  accessToken: '23bd92fae990454b8789ad3be0601895' /* process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN */,
-  environment: 'production',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-};
+import init from './init';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <StoreProvider store={store}>
-          <BrowserRouter>
-            <I18nextProvider i18n={i18next}>
-              <App />
-            </I18nextProvider>
-          </BrowserRouter>
-        </StoreProvider>
-      </ErrorBoundary>
-    </RollbarProvider>
-  </React.StrictMode>,
-);
+root.render(init());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
