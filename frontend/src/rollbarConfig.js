86542d0907dc8d2eb758d913a9fdaaffd539c4d3
Yuriy Bondaryuk <ybondaryuk@marfa-tech.com>
2024-08-22 23:25:19 +0300

    main review: fix 7 - secret token

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,10 +1,12 @@
-const rollbarConfig = {
+import Rollbar from 'rollbar';
+
+const rollbarConfig = new Rollbar({
   // accessToken: process.env.post_client_item /* '23bd92fae990454b8789ad3be0601895' */,
   accessToken: process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN,
   environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
-};
+});
 
 console.log('process.env', process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN);
 console.log('process.env', process.env);
