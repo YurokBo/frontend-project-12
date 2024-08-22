1469d22671994a707f85dee25f57fa004025ca5a
Yuriy Bondaryuk <ybondaryuk@marfa-tech.com>
2024-08-22 23:32:10 +0300

    main review: fix 7 - secret token

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,14 +1,13 @@
-import Rollbar from 'rollbar';
-
-const rollbarConfig = new Rollbar({
+const rollbarConfig = {
   // accessToken: process.env.post_client_item /* '23bd92fae990454b8789ad3be0601895' */,
-  accessToken: process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN,
+  accessToken: 'REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN',
   environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
-});
+};
 
 console.log('process.env', process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN);
-console.log('process.env', process.env);
+console.log('rollbarConfig', rollbarConfig);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 export default rollbarConfig;
