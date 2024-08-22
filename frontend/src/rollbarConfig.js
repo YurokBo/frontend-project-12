fd6ef806695b0542385001cac741d46feee905d3
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-08-22 23:07:11 +0300

    main review: fix 7 - secret token

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,11 +1,11 @@
 const rollbarConfig = {
   // accessToken: process.env.post_client_item /* '23bd92fae990454b8789ad3be0601895' */,
-  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
+  accessToken: process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN,
   environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
 };
 
-console.log('process.env', process.env);
+console.log('process.env', process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN);
 
 export default rollbarConfig;
