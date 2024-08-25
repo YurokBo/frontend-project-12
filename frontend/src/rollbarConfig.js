27b78bb65700147d4f7e02e3297bc0eac0fca994
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-08-25 18:16:30 +0300

    main review: fix 7 - secret token

--- frontend/src/rollbarConfig.js
+++ frontend/src/rollbarConfig.js
@@ -1,12 +1,13 @@
 const rollbarConfig = {
-  // accessToken: process.env.post_client_item /* '23bd92fae990454b8789ad3be0601895' */,
-  accessToken: process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN,
+  // accessToken: process.env.REACT_APP_NOT_SECRET_CODE /* '23bd92fae990454b8789ad3be0601895' */,
+  accessToken: process.env.REACT_APP_NOT_SECRET_CODE,
   environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
 };
 
 console.log('REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN', process.env.REACT_APP_POST_CLIENT_ITEM_ACCESS_TOKEN);
+console.log('REACT_APP_NOT_SECRET_CODE', process.env.REACT_APP_NOT_SECRET_CODE);
 console.log('rollbarConfig', rollbarConfig);
 
 export default rollbarConfig;
